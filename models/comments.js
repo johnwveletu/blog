9ff5add0db549ba94d658da8514aa8933d23f61f
johnwveletu <johnwveletu@yahoo.com>
2017-09-29 16:26:10 +0800

    commit  all blog

---
+++ models/comments.js
@@ -0,0 +1,42 @@
+var Comment = require('../lib/mongo').Comment;
+var marked = require('marked');
+
+//将comment 的content 从markdown 转换成 html 这里我们为comment添加了一个插件
+Comment.plugin('contentToHtml',{
+    afterFind:function(comments){
+        return comments.map(function(comment){
+            comment.content = marked(comment.content);
+            return comment;
+        });
+    }
+});
+
+module.exports = {
+    //创建一个留言
+    create:function create(comment){
+        return Comment.create(comment).exec();
+    },
+    //通过用户id和留言id删除一个留言
+    delCommentById:function delCommentById(author,commentId){
+        return Comment.remove({author:author,_id:commentId}).exec();
+    },
+    //通过文章id删除该文章下的所有留言
+    delCommentByPostId:function delCommentByPostId(postId){
+        return Comment.remove({postId:postId}).exec();
+    },
+    //通过文章id获取文章下的所有留言，按留言创建时间升序
+    getComment:function getComment(postId){
+        return Comment.find({postId:postId})
+        .populate({path:'author',model:'User'})//此函数的作用是？？？？？？？？？？
+        .sort({_id:1})
+        .addCreateAt()
+        .contentToHtml()
+        .exec();
+    },
+     // 通过文章 id 获取该文章下留言数
+    getCommentsCount: function getCommentsCount(postId) {
+        return Comment.count({ postId: postId }).exec();
+    }
+    
+    
+};
---
+++ models/posts.js
@@ -0,0 +1,92 @@
+var Post = require('../lib/mongo').Post;
+var marked = require('marked');
+var CommentModel = require('./comments');
+
+//添加留言数量 添加一个插件 注意插件只有afterFind 和 afterFindOne
+Post.plugin('addCommentsCount',{
+    afterFind:function(posts){
+        return Promise.all(post.map(function(post){
+            return CommentModel.getCommentsCount(post._id).then(function(commentCounts){
+                post.commentCounts = commentCounts;
+                return post;
+            });
+        }));
+    },
+    afterFindOne:function(post){
+        if(post){
+            return CommentModel.getCommentsCount(post._id).then(function(count){
+                post.commentCounts = count;
+                return post;
+            });
+        }
+        return post;
+    }
+});
+// 将 post 的 content 从 markdown 转换成 html
+Post.plugin('contentToHtml', {
+    afterFind: function (posts) {
+      return posts.map(function (post) {
+        post.content = marked(post.content);
+        return post;
+      });
+    },
+    afterFindOne: function (post) {
+      if (post) {
+        post.content = marked(post.content);
+      }
+      return post;
+    }
+  });
+
+
+module.exports = {
+    //创建一篇文章
+    create:function(post){
+        return Post.create(post).exec();
+    },
+    //通过文章id获取一篇文章
+    getPostById:function(postId){
+        return Post.findOne({_id:postId})
+        .populate({path:'author',model:'User'})
+        .addCreateAt()
+        .contentToHtml()
+        .exec();
+    },
+    //按创建时间降序获取所有用户文章或者某个特定用户的文章
+    getPosts:function(author){
+        var query = {};
+        if(author){
+            query.author = author;
+        }
+        return Post.find(query)
+        .populate({path:'author',model:'User'})
+        .sort({_id:-1})
+        .addCreateAt()
+        .addCommentsCount()
+        .contentToHtml()
+        .exec();
+    },
+    incPv:function(postId){
+        return Post.update({_id:postId},{$inc:{pv:1}})
+        .exec();
+    },
+    getRawPostById:function(postId){
+        return Post.findOne({_id:postId})
+        .populate({path:'author',model:'User'})
+        .exec();
+    },
+    updatePostById:function(postId,author){
+        return Post.update({author:author,_id:postId},{$set:{data}})
+        .exec();
+    },
+    delPostById:function(author,postId){
+        return Post.remove({author:author,_id:postId})
+        .exec()
+        .then(function(res){
+            //同时删除该文章下的留言
+            if(res.result.ok && res.result.n > 0){
+                return CommentModel.delCommentByPostId(postId);
+            }
+        })
+    }
+}
