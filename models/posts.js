5209d8814bec5f108dda6566ee19da25ba158d10
whx <452854951@qq.com>
2018-01-31 21:42:05 +0800

    美化代码排版

--- models/posts.js
+++ models/posts.js
@@ -3,18 +3,18 @@ var marked = require('marked');
 var CommentModel = require('./comments');
 
 //添加留言数量 添加一个插件 注意插件只有afterFind 和 afterFindOne
-Post.plugin('addCommentsCount',{
-    afterFind:function(posts){
-        return Promise.all(posts.map(function(post){
-            return CommentModel.getCommentsCount(post._id).then(function(commentCounts){
+Post.plugin('addCommentsCount', {
+    afterFind: function (posts) {
+        return Promise.all(posts.map(function (post) {
+            return CommentModel.getCommentsCount(post._id).then(function (commentCounts) {
                 post.commentCounts = commentCounts;
                 return post;
             });
         }));
     },
-    afterFindOne:function(post){
-        if(post){
-            return CommentModel.getCommentsCount(post._id).then(function(count){
+    afterFindOne: function (post) {
+        if (post) {
+            return CommentModel.getCommentsCount(post._id).then(function (count) {
                 post.commentCounts = count;
                 return post;
             });
@@ -25,68 +25,99 @@ Post.plugin('addCommentsCount',{
 // 将 post 的 content 从 markdown 转换成 html
 Post.plugin('contentToHtml', {
     afterFind: function (posts) {
-      return posts.map(function (post) {
-        post.content = marked(post.content);
-        return post;
-      });
+        return posts.map(function (post) {
+            post.content = marked(post.content);
+            return post;
+        });
     },
     afterFindOne: function (post) {
-      if (post) {
-        post.content = marked(post.content);
-      }
-      return post;
+        if (post) {
+            post.content = marked(post.content);
+        }
+        return post;
     }
-  });
+});
 
 
 module.exports = {
     //创建一篇文章
-    create:function(post){
+    create: function (post) {
         return Post.create(post).exec();
     },
     //通过文章id获取一篇文章
-    getPostById:function(postId){
-        return Post.findOne({_id:postId})
-        .populate({path:'author',model:'User'})
-        .addCreateAt()
-        .contentToHtml()
-        .exec();
+    getPostById: function (postId) {
+        return Post.findOne({
+                _id: postId
+            })
+            .populate({
+                path: 'author',
+                model: 'User'
+            })
+            .addCreateAt()
+            .contentToHtml()
+            .exec();
     },
     //按创建时间降序获取所有用户文章或者某个特定用户的文章
-    getPosts:function(author){
+    getPosts: function (author) {
         var query = {};
-        if(author){
+        if (author) {
             query.author = author;
         }
         return Post.find(query)
-        .populate({path:'author',model:'User'})
-        .sort({_id:-1})
-        .addCreateAt()
-        .addCommentsCount()
-        .contentToHtml()
-        .exec();
+            .populate({
+                path: 'author',
+                model: 'User'
+            })
+            .sort({
+                _id: -1
+            })
+            .addCreateAt()
+            .addCommentsCount()
+            .contentToHtml()
+            .exec();
     },
-    incPv:function(postId){
-        return Post.update({_id:postId},{$inc:{pv:1}})
-        .exec();
+    incPv: function (postId) {
+        return Post.update({
+                _id: postId
+            }, {
+                $inc: {
+                    pv: 1
+                }
+            })
+            .exec();
     },
-    getRawPostById:function(postId){
-        return Post.findOne({_id:postId})
-        .populate({path:'author',model:'User'})
-        .exec();
+    getRawPostById: function (postId) {
+        return Post.findOne({
+                _id: postId
+            })
+            .populate({
+                path: 'author',
+                model: 'User'
+            })
+            .exec();
     },
-    updatePostById:function(postId,author){
-        return Post.update({author:author,_id:postId},{$set:{data}})
-        .exec();
+    updatePostById: function (postId, author) {
+        return Post.update({
+                author: author,
+                _id: postId
+            }, {
+                $set: {
+                    data
+                }
+            })
+            .exec();
     },
-    delPostById:function(author,postId){
-        return Post.remove({author:author,_id:postId})
-        .exec()
-        .then(function(res){
-            //同时删除该文章下的留言
-            if(res.result.ok && res.result.n > 0){
-                return CommentModel.delCommentByPostId(postId);
-            }
-        })
+    delPostById: function (author, postId) {
+        return Post.remove({
+                author: author,
+                _id: postId
+            })
+            .exec()
+            .then(function (res) {
+                //同时删除该文章下的留言
+                if (res.result.ok && res.result.n > 0) {
+                    return CommentModel.delCommentByPostId(postId);
+                }
+            })
     }
 }
